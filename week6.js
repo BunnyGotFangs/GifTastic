3b49a3f8c37859101516cc7a603fea2d74b627e6
BunnygotFangs <kimberly@thehilts.com>
2018-04-21 06:49:55 -0500

    commiting updates where no longer persists but code still speaking site for getting data

--- week6.js
+++ week6.js
@@ -1,50 +1,56 @@
 // desclaration of needed global variables including starting buttons, apikey, url
-var poisons = ["Hemlock","Belladonna","Textrodotoxin","Mercury","Cyanide"];
+var topic = [];
 var apiKey = "Atz4BqbSfFySR78ltn4KnkbuESh3unL4";
-var gifUrl = "http://api.giphy.com/v1/gifs/search?";
-var queryUrl = gifUrl + "api_key=" + apiKey + "&limit=10" + "&q="; 
+var gifUrl = "https://api.giphy.com/v1/gifs/search?";
+var queryUrl = gifUrl + "api_key=" + apiKey  + "&limit=10" + "&q=" ; 
 
 console.log("Making sure html is reaching file");
 
 
 //need to have a function created now so that this can be called over and over again 
-$("#gif-input").on("click", function() {
+function searchGif(poisons) {
+$("button").on("click", function() {
 // desclared global, but reviewing back seems to need local so creting local = to global 
 // know I need more local variables, not quite sure what they are yet, so will add as needed  
-  var animal = $(this).attr("gif-name");
-  var localURL = queryUrl;
-
+  var poisonInfo = $(this).attr("poison");
+  var localURL = queryUrl + posionInfo;
+//((UGH Still not getting this to work))
+console.log(localURL);
   // Performing an AJAX request with the queryURL
   $.ajax({
-    url: queryURL,
+    url: localURL,
     method: "GET"
   })
 // need to capture the information coming back from the website, so the first one is the function to capture info from API call
       .then(function(response) {
 //figuring out if it reading my stuff      
-      console.log(queryURL);
+      console.log(localURL);
 //checking to see if I have any response ie is my API working?
       console.log(response);
 //now defining  variables for specific data points
       var results = response.data;
 
 //not quite sure if this is where my loop goes, but purpose is to go through the array to create buttons in the array      
-      for (var i = 0; i < poisons.length; i++) {
+      for (var i = 0; i < topic.length; i++) {
 //now need to figure out how to add the buttons to the html page and want them and the attributes associated to the buttons to go to the selections division from the HTML page
 // div      
-      var poisonDiv = $("<div>");
+      var poisonDiv = $("<div>");    
+
 //naming of the results      
-      var p = $("<p>").text("Rating: " + results[i].rating);
+      var pCap = $("<p>").text("Caption: " + results[i].caption);      
+      var pRat = $("<p>").text("Rating: " + results[i].rating);
+   
 //for the image/video being stored
       var poisonImage = $("<img>");
 //trying to size the result so it looks ok on the page
       poisonImage.attr("src", results[i].images.fixed_height.url);
 //now changing the index.html so the new buttons are visible and selectable
-        poisonDiv.append(p);
+        poisonDiv.append(pCap, pRat);
         poisonDiv.append(poisonImage);
         $("#selection").prepend(poisonDiv);}
-      })
+      
     });
+});}
 // next section is to be able to add new button from the input poison
 
 // lastly need to clear the results panel, when a new button is click before we load new images
