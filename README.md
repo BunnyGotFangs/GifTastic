2c5786424d0c3a7a2ce16a38e227493f9ab39a73
BunnygotFangs <kimberly@thehilts.com>
2018-04-20 23:37:52 -0500

    update of the JS with the result capture, not working have an error that have spent two hours on trying, 100K ft psuedo coding, adding read me

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # GifTastic
 Homework 6 - API dynamic page
+
+
+WHAT THE PROJECT DOES:
+The project is a test of one's ability or in my case NOT ability for how to connect to another source and pull data into my site. 
+
+In this case we will have an API to giphy, and will pull informtion back (10 results). The results will be based upon the button the person selects. 
+
+The buttons are an array and the array can be added to.
+
+
+
+WHY IT IS USEFUL:
+
+Other can use the code when working to build or test their API to the giphy site, they will need their own unique key preferrable, but gain the code cn be used to folks testing or building their own little repo.
+
+
+
+START FROM HERE: 
+
+    1. click any button on the page and you should get 10 results
+    2. input info into the whats your poison and click the Add Now 
+            new button should be created
+    3. repeat step 1
+    4. when you refresh, the page will have the initil button, the results will clear and you can begin again
+    5. you can add as many button as you would like
+
+To get to the game copy the URL below and paste into your browser.
+
+    https://bunnygotfangs.github.io/GifTastic/
+
+
+NEED HELP or HAVE SUGGESTIONS:
+
+    Have questions or need help with the code, the game mechanics or have suggestions then please feel free to contact me at my website:
+
+    https://bunnygotfangs.github.io/Bootstrap-Portfolio/contact.html
+
+CONTRIBUTIONS:
+
+    Many thanks to all the trailblazers before me who actually got their attack to work right! To those who survived coding BootCamp! -- I truly hope you have landed where you want to be as this is a journey, a sprint, a marathon, and an F&*$'in HILL CLIMB! 
+
+    Thanks for having a sense of humor!
--- week6.js
+++ week6.js
@@ -1,22 +1,56 @@
 // desclaration of needed global variables including starting buttons, apikey, url
-var poison = ["Hemlock","Belladonna","Textrodotoxin","Mercury","Cyanide"];
+var poisons = ["Hemlock","Belladonna","Textrodotoxin","Mercury","Cyanide"];
 var apiKey = "Atz4BqbSfFySR78ltn4KnkbuESh3unL4";
-var gifUrl = "https://api.giphy.com/v1/gifs/random";
-var queryUrl = gifUrl + "?api_key=" + apiKey+"tag="; 
+var gifUrl = "http://api.giphy.com/v1/gifs/search?";
+var queryUrl = gifUrl + "api_key=" + apiKey + "&limit=10" + "&q="; 
 
-//connecting to giphy to get content
+console.log("Making sure html is reaching file");
 
-function gettingGiphys() {
 
-  var gipghy = $(this).attr("results");
+//need to have a function created now so that this can be called over and over again 
+$("#gif-input").on("click", function() {
 // desclared global, but reviewing back seems to need local so creting local = to global 
 // know I need more local variables, not quite sure what they are yet, so will add as needed  
-  var localUrl = queryUrl;
+  var animal = $(this).attr("gif-name");
+  var localURL = queryUrl;
 
-//creating the get statement decalaration so I can pull the need images need to link to a button name to input the tag variable UGH
+  // Performing an AJAX request with the queryURL
   $.ajax({
-    url: localUrl,
+    url: queryURL,
     method: "GET"
-  }).then(function(response) {
-      
-)}
+  })
+// need to capture the information coming back from the website, so the first one is the function to capture info from API call
+      .then(function(response) {
+//figuring out if it reading my stuff      
+      console.log(queryURL);
+//checking to see if I have any response ie is my API working?
+      console.log(response);
+//now defining  variables for specific data points
+      var results = response.data;
+
+//not quite sure if this is where my loop goes, but purpose is to go through the array to create buttons in the array      
+      for (var i = 0; i < poisons.length; i++) {
+//now need to figure out how to add the buttons to the html page and want them and the attributes associated to the buttons to go to the selections division from the HTML page
+// div      
+      var poisonDiv = $("<div>");
+//naming of the results      
+      var p = $("<p>").text("Rating: " + results[i].rating);
+//for the image/video being stored
+      var poisonImage = $("<img>");
+//trying to size the result so it looks ok on the page
+      poisonImage.attr("src", results[i].images.fixed_height.url);
+//now changing the index.html so the new buttons are visible and selectable
+        poisonDiv.append(p);
+        poisonDiv.append(poisonImage);
+        $("#selection").prepend(poisonDiv);}
+      })
+    });
+// next section is to be able to add new button from the input poison
+
+// lastly need to clear the results panel, when a new button is click before we load new images
+  
+// Now there needs to be a function created that we when the buttons are clicked that it will call the query, hit the results and publish them and this needs to be repeatable 
+
+// next need to be able to be able to stop and start the giphy
+
+// lastly need to clear the results panel, when a new button is click before we load new images
